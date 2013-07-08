11085ebbba5e98bce61347a9eca9c0f3e1eb89ad
Zach Mullen <zach.mullen@kitware.com>
2013-07-08 18:40:05 -0400

    ENH: Cool endpoint wrapper to auto-serialize output

--- server/main.py
+++ server/main.py
@@ -19,52 +19,68 @@ class Root():
 class Node():
     pass
 
-class DBResource:
+class RestResource:
     def __init__(self, _conn):
         self.conn = _conn
         self.simcollection = self.conn.cosmodata.simulations
 
-class Simulation(DBResource):
+    def simplify(self, obj):
+        return obj
+
+    @classmethod
+    def endpoint(cls, fun):
+        def wrapper(self, *args, **kwargs):
+            val = fun(self, *args, **kwargs)
+
+            accepts = cherrypy.request.headers.elements('Accept')
+            for accept in accepts:
+                if accept.value == 'application/json':
+                    break
+                elif accept.value == 'text/html':
+                    # Pretty-print and HTMLify the response for display in browser
+                    resp = json.dumps(val, indent=4, sort_keys=True, separators=(',', ': '))
+                    resp = resp.replace(' ', '&nbsp;').replace('\n', '<br />')
+                    resp = '<div style="font-family: monospace">' + resp + '</div>'
+                    return resp
+
+            #Default behavior will just be normal JSON output
+            return json.dumps(val)
+        return wrapper
+
+class Simulation(RestResource):
     exposed = True
 
+    def simplify(self, doc):
+        return {'id': str(doc['_id']),
+                'name': doc['simulation_name'],
+                'site': doc['source']['site'],
+                'user': doc['source']['user']}
+
+    @RestResource.endpoint
     def GET(self, simid=None):
         result = None
-
-        def simplify(doc):
-            return {'id': str(doc['_id']),
-                    'name': doc['simulation_name'],
-                    'site': doc['source']['site'],
-                    'user': doc['source']['user']}
 
         if simid == None:
             # Return list of simulations
             simulations = self.simcollection.find()
-                                                  # {"_id": True,
-                                                  #  "source": True,
-                                                  #  "analysistool": True})
-
-
-            result = [simplify(doc) for doc in simulations]
+            result = [self.simplify(doc) for doc in simulations]
         else:
             s = self.simcollection.find_one({'_id' : ObjectId(simid)})
+            result = self.simplify(s)
 
-            result = simplify(s)
+        return result
 
-        return json.dumps(result)
-
-class Viewable(DBResource):
+class Viewable(RestResource):
     exposed = True
 
+    @RestResource.endpoint
     def GET(self, **params):
         simid = params['simulation_id']
 
         s = self.simcollection.find_one({'_id' : ObjectId(simid)})
         s = s['cosmo']['analysistool']
 
-        def simplify():
-            pass
-
-        return json.dumps(s)
+        return self.simplify(s)
 
 
 if __name__ == '__main__':
