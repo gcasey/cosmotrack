4987be73ae675d1bebe042249a56dcc0fd29b59a
Casey Goodlett <casey.goodlett@kitware.com>
2013-07-08 17:19:42 -0400

    FIX: Server index.html as root

--- server/main.py
+++ server/main.py
@@ -4,9 +4,21 @@ from bson.objectid import ObjectId
 import json
 import os
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 class Root():
+    exposed = True
+
     def __init__(self):
         self.conn = pymongo.Connection()
+
+    def GET(self):
+        print '*******'
+        return cherrypy.lib.static.serve_file(os.path.join(ROOT_DIR, 'static', 'index.html'),
+                                              content_type='text/html')
+
+class Node():
+    pass
 
 class DBResource:
     def __init__(self, _conn):
@@ -58,21 +70,26 @@ class Viewable(DBResource):
 
 if __name__ == '__main__':
     root = Root()
-    root.api = Root()
-    root.api.v1 = Root()
+    root.api = Node()
+    root.api.v1 = Node()
 
     apiv1 = root.api.v1
     apiv1.simulation = Simulation(root.conn)
     apiv1.viewable = Viewable(root.conn)
 
-    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     config = {
-        '/' : {'request.dispatch' : cherrypy.dispatch.MethodDispatcher(),
-               'tools.staticdir.root' : rootDir},
-        '/index.html' : {'tools.staticfile.on' : True,
-                         'tools.staticfile.filename' : '%s/static/index.html' % rootDir},
-        '/static' : {'tools.staticdir.on' : True,
-                     'tools.staticdir.dir' : 'static'}
+        '/' : {
+            'request.dispatch' : cherrypy.dispatch.MethodDispatcher(),
+            'tools.staticdir.root' : '%s/static' % ROOT_DIR
+            },
+        '/app' : {
+            'tools.staticdir.on' : 'True',
+            'tools.staticdir.dir' : 'app',
+            },
+        '/lib' : {
+            'tools.staticdir.on' : 'True',
+            'tools.staticdir.dir' : 'lib',
+            }
         }
 
     cherrypy.tree.mount(root, '/', config)
