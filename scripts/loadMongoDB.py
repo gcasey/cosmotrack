966a054254b594c0fd93ac36f7b4b61ce3a1de15
Casey Goodlett <casey.goodlett@kitware.com>
2013-07-09 14:34:11 -0400

    FIX: Switch analysis tools to an array in the database

--- scripts/loadMongoDB.py
+++ scripts/loadMongoDB.py
@@ -3,6 +3,7 @@ import configProcessors
 import sys
 import glob
 import itertools
+import pprint
 
 try:
     from pymongo import MongoClient
@@ -19,18 +20,21 @@ if __name__ == "__main__":
     document = configParsers.main(sys.argv[1], sys.argv[2], sys.argv[3])
     document = configProcessors.process_timesteps(document)
     document = configProcessors.idify_analysis(document)
+    document = configProcessors.flattenAssociativeArray(document, 'cosmo', 'analysistool')
 
     # HACK in extra data that we might need
     document['source'] = {'site' : 'localhost',
                           'user' : 'casey.goodlett'}
-    
+
     # HACK hard-coded
     toolstoadd = ['tess', 'halotracker_bb_0_2_']
     extensions = ['*.vtp', '*.vtu', '*.vtm']
 
     # Lets setup some output files
     # Loop over analysis tools
-    for name, info in document['cosmo']['analysistool'].iteritems():
+    for info in document['cosmo']['analysistool']:
+        name = info['key']
+
         if name in toolstoadd:
             outputdir = info['base_output_file_name']
             files = list(itertools.chain.from_iterable(glob.glob('%s*%s' % (outputdir, ext)) for ext in extensions))
@@ -39,5 +43,5 @@ if __name__ == "__main__":
             # Warning this is a big hack that assumes the ordering of these two things are the same
             info['files'] = zip(timesteps, files)
 
-
+    pprint.pprint(document)
     db.simulations.insert(document)
