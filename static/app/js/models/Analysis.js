817a2b553ea1df0684cbb992ae885f0a3efde4d6
Casey Goodlett <casey.goodlett@kitware.com>
2013-07-15 10:15:40 -0400

    ENH: Add validation method for analysis model

--- static/app/js/models/Analysis.js
+++ static/app/js/models/Analysis.js
@@ -1,5 +1,11 @@
 ct.models.Analysis = Backbone.Model.extend({
     id: null,
     name: null,
-    loadDataArgs: null
+    loadDataArgs: null,
+
+    validate : function(attrs, options) {
+        if (attrs.loadDataArgs === null) {
+            return 'Load data not specified';
+        }
+    }
 });
